34f07e0ec4bf5d12ff209c0836968479ebc87267
kirilloid173 <kirilloid173@gmail.com>
2019-12-06 14:22:09 +0400

    0.2

--- Resolution.php
+++ Resolution.php
@@ -2,5 +2,26 @@
 $a=$_POST['a'];
 $b=$_POST['b'];
 $c=$_POST['c'];
-
+echo "<script>console.log('$a'); console.log('$b'); console.log('$c');</script>";
+$D=((($b*$b)))-(((4*$a)*$c));
+echo "<script>console.log('d= $D');</script>";
+if ($D<=0){
+    echo "<p style='margin-top:250px; font-size:32px; color:red; text-align:center'>D Can`t be <=0!</p>";
+}
 ?>
+<body>
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB"
+        crossorigin="anonymous">
+
+<form action="After_login.html" method="POST">
+
+<input type="submit" class="btn btn-success" style="margin-left:730px;" value="Return"/>
+</form>
+
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
+        crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49"
+        crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js" integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T"
+        crossorigin="anonymous"></script>
+</body>
